8a7e4adb768b749a74d092bda1b0e1ffd8ba79c5
Maedeh Dehghan <maedehdehghanam@gmail.com>
2022-01-23 22:25:33 +0330

    add insert and search method to kd tree

---
+++ KDTree.java
@@ -0,0 +1,5 @@
+public class KDTree{
+	public Node root = null ;
+    public static int k = 2 ;
+
+}
---
+++ Node.java
@@ -0,0 +1,6 @@
+public class Node{
+	private Object place ;
+    public Coordination coordination ;
+    public Node left = null ;
+    public Node right = null ;
+}
